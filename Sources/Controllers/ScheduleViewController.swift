25bf1c918abfa1c5a9e90bc8d73db48aa7708460
Nick Podratz <nick@podratz.de>
2017-05-02 23:35:14 +0200

    Use Fiori cells in ScheduleVC

--- Sources/Controllers/ScheduleViewController.swift
+++ Sources/Controllers/ScheduleViewController.swift
@@ -7,15 +7,21 @@
 //
 
 import UIKit
+import SAPFiori
 
 class ScheduleViewController: UITableViewController {
 
     var detailViewController: MapViewController? = nil
     var objects = [Any]()
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(FUITimelineCell.self, forCellReuseIdentifier: FUITimelineCell.reuseIdentifier)
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.backgroundColor = UIColor.preferredFioriColor(forStyle: .backgroundBase)
+        tableView.separatorStyle = .none
+
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
 
@@ -67,11 +73,20 @@ class ScheduleViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: FUITimelineCell.reuseIdentifier, for: indexPath)
+        guard let timelineCell = cell as? FUITimelineCell else { return cell }
+        //        let object = objects[indexPath.row]
+        timelineCell.timelineWidth = CGFloat(95.0)
+        timelineCell.headlineText = "Headline"// ticket.title
+        timelineCell.subheadlineText = "id: 1234" //ticket.productid
+        timelineCell.nodeImage = FUITimelineNode.open
+        timelineCell.eventText =  "10:00 AM"
+        timelineCell.eventImage =  UIImage() // TODO: Replace with your image
+        timelineCell.statusImage = UIImage() // TODO: Replace with your image
+        timelineCell.subStatusText =  "rainy"
+        timelineCell.attributeText =  "1h 30 min"
+        timelineCell.subAttributeText =  "8 min (2.4 min)"
+        return timelineCell
     }
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
